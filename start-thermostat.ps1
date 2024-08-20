f057a06ad9d6fccd21110c85d03abc39639de5b1
Logan George <logangeorge01@gmail.com>
2024-08-19 18:37:07 -0700

    change2

--- start-thermostat.ps1
+++ start-thermostat.ps1
@@ -1,16 +1,17 @@
 param(
     [string]$RunAsProcess = "false",
-    [string]$BuildExe = "true"
+    [string]$BuildExe = "true",
+    [string]$BuildPath = "."
 )
 
 if ($BuildExe -eq "true") {
     Write-Host "Building thermostat controller"
     go1.22.5 mod download
-    go1.22.5 build -o thermostat.exe -v
+    go1.22.5 build -o $BuildPath\thermostat.exe -v
 }
 
 Write-Host "Running thermostat controller"
-$cmd = ".\thermostat.exe"
+$cmd = "$BuildPath\thermostat.exe"
 if ($RunAsProcess -eq "true") {
     $cmd = "Start-Process -FilePath $cmd"
 }
